373799bfe51b222618ae9a32761bd2c68c88b2f5
heuzin <matheusqs.arq@gmail.com>
2020-12-01 15:14:27 -0500

    showing quotes and author to page, adding fucntionality to buttons

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const quoiteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
 // Get Quote From API
 async function getQuote() {
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
@@ -5,12 +12,35 @@ async function getQuote() {
     try {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
-        console.log(data)
+        // If Autho is blank, add 'Unknown'
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown'
+        } else {
+            authorText.innerText = data.quoteAuthor
+        }
+        // Reduce fonr size for long quotes
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote')
+        }
+        quoteText.innerText = data.quoteText
     } catch (error) {
-        // getQuote();
-        console.log('whoops, no quote', error);
+        getQuote();
     }
 }
+
+// Tweet Quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twittherUrls = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twittherUrls, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote)
+twitterBtn.addEventListener('click', tweetQuote)
 
 // On Load
 getQuote();
