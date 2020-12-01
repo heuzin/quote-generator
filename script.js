40d9d53e204026b877ac691aec12de889fceb134
heuzin <matheusqs.arq@gmail.com>
2020-12-01 15:28:51 -0500

    renaming

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
 // Show Loading
-function loading() {
+function showLoadingSpinner() {
     loader.hidden = false;
     quoteContainer.hidden = true;
 }
 
 // Hide Loading
-function complete() {
+function removeLoadingSpinner() {
     if (!loader.hidden) {
         quoteContainer.hidden = false
         loader.hidden = true
@@ -21,7 +21,8 @@ function complete() {
 
 // Get Quote From API
 async function getQuote() {
-    loading();
+    showLoadingSpinner();
+    // We need to use a Proxy URL to make our API call 
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
@@ -41,7 +42,7 @@ async function getQuote() {
         }
         quoteText.innerText = data.quoteText
         // Sopt Loader, and show the Quote
-        complete();
+        removeLoadingSpinner();
     } catch (error) {
         getQuote();
     }
